88ab679b59d6349e330d6ccf2217b8b8e01d98b0
vsuaste <suaste@cimat.mx>
2018-03-09 11:30:01 -0600

    direct resolvers

--- resolvers.js
+++ resolvers.js
@@ -2,32 +2,42 @@
   Resolvers for CRUD basic operations
   given a model defined with sequelize
 */
-
- var Crud = require('./crud');
  const{person} = require('./model');
-
- const crud = new Crud(person);
 
  module.exports = {
 
    people: function(){
-    return crud.allItems();
+    return person.findAll();
   },
 
   readOne: function({id}){
-    return crud.itemById({id});
+    return person.findOne({where: {id: id}});
   },
 
-  addPerson: function(args){
-    return crud.addItem(args);
+  addPerson: function({firstName,lastName,email}){
+      return person.create({firstName, lastName, email});
   },
 
   deletePerson: function({id}){
-    return crud.deleteItem({id});
+    return person
+      .findById(id)
+      .then( person =>{
+          return person
+          .destroy()
+          .then(()=>{return 'Item succesfully deleted';});
+      });
   },
 
-  updatePerson: function(args){
-    return crud.updateItem(args);
+  updatePerson: function({id,firstName,lastName,email}){
+    return person
+    .findById(id)
+    .then( person => {
+      return person.update({
+        firstName: firstName || person.firstName,
+        lastName: lastName || person.lastName,
+        email: email || person.email,
+      })
+    });
   }
-
- }
+  
+}
