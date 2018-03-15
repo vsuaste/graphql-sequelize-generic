cadf5dbc1e2717ae9dd0e6c07c713757a51bcf2d
vsuaste <suaste@cimat.mx>
2018-03-15 13:22:55 -0600

    utils createdwith searchArg

--- resolvers.js
+++ resolvers.js
@@ -5,60 +5,9 @@
  const{person} = require('./models/model');
  const jwt =  require('jsonwebtoken');
  const bcrypt = require('bcryptjs');
+ const searchArg = require('./utils/search-argument');
 
  var secret = 'something-secret';
-
-
- class searchArg{
-   constructor({field, value, operator, searchArg}){
-     this.field = field;
-     this.value = this.constructor.parseValue(value);
-     this.operator = operator;
-     this.searchArg = searchArg
-   }
-
-   static parseValue(val){
-     if(val!==undefined)
-     {
-       if(val.type === "Array")
-       {
-         return val.value.split(",");
-       }else{
-         return val.value;
-       }
-     }
-   }
-
-   toSequelize(){
-     let searchArgsInSequelize = {};
-
-     if(this.searchArg === undefined && this.field === undefined)
-     {
-       searchArgsInSequelize['$'+this.operator] = this.value;
-
-     }else if(this.searchArg === undefined)
-     {
-       searchArgsInSequelize[this.field] = {
-          ['$'+this.operator] : this.value
-       };
-     }else if(this.field === undefined){
-       searchArgsInSequelize['$'+this.operator] = this.searchArg.map(sa => {
-         let new_sa = new searchArg(sa);
-         return new_sa.toSequelize();
-       });
-     }else{
-        searchArgsInSequelize[this.field] = {
-          ['$'+this.operator] : this.searchArg.map(sa => {
-            let new_sa = new searchArg(sa);
-            return new_sa.toSequelize();
-          })
-        }
-     }
-
-     return searchArgsInSequelize;
-   }
- };
-
 
  module.exports = {
 
